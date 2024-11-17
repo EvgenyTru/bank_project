c3851a1ddee2c3b3db747bd0b0ecf3c6ca1aa280
Evgeny <evgeny.trufanov1984@gmail.com>
2024-11-17 18:31:35 +0400

    debug widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,12 @@
 from masks import get_mask_card_number, get_mask_account
 
-def mask_account_card(bank_info: str) -> str:
+
+def mask_account_card(bank_info: str) -> None:
     """Маскировка номера карты либо счета"""
     if "Счет" in bank_info:
-        return get_mask_account(bank_info)
+        return "Счет" + " " + get_mask_account(bank_info[5:])
     else:
-        cards = get_mask_card_number(bank_info[-16:])
-        masked_cards = bank_info.replace(bank_info[-16:], bank_info)
-        return masked_cards
+        return f"""{bank_info[:-16]} {get_mask_card_number(bank_info[-16:])}"""
 
 
 def get_date(date: str) -> str | None:
