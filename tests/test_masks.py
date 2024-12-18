50c11919186f2958d51926907d2365df2b429387
Evgeny <evgeny.trufanov1984@gmail.com>
2024-12-18 19:14:19 +0400

    final version test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,17 +4,15 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_get_mask_card_number(card_numbers_16):
-    assert get_mask_card_number(card_numbers_16) == "7000 79** **** 6361"
+    assert get_mask_card_number(card_numbers_16) == '7000 79** **** 6361'
 
 
 def test_get_mask_card_numbers_with_spaces(card_numbers_16_space):
-    with pytest.raises(AssertionError) as exc_info:
-        assert str(exc_info.value) == "Не верный формат"
+    assert get_mask_card_number(card_numbers_16_space) == 'Не корректный номер'
 
 
 def test_get_mask_card_numbers_alpha(card_numbers_alpha):
-    with pytest.raises(AssertionError) as exc_info:
-        assert str(exc_info.value) == "Не верный формат"
+    assert get_mask_card_number(card_numbers_alpha) == 'Не корректный номер'
 
 
 def test_get_mask_account_base(count_numbers_20):
@@ -22,9 +20,8 @@ def test_get_mask_account_base(count_numbers_20):
 
 
 def test_get_mask_account_spaces(count_numbers_20_spaсes):
-    assert get_mask_account(count_numbers_20_spaсes) == "**1234"
+    assert get_mask_account(count_numbers_20_spaсes) == 'Не корректный номер'
 
 
 def test_get_mask_account_alpha(count_numbers_alpha):
-    with pytest.raises(AssertionError) as exc_info:
-        assert str(exc_info.value) == "Не верный формат"
+    assert get_mask_account(count_numbers_alpha) == 'Не корректный номер'
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.widget import mask_account_card, get_date
 
+
 @pytest.mark.parametrize(
     'value, expected',
     [
@@ -14,3 +15,39 @@ from src.widget import mask_account_card, get_date
 )
 def test_mask_account_card(value, expected):
     assert mask_account_card(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("Visa Platinum ", "номер карты не указан"),
+        ("Счет ", "номер счета не указан"),
+        ("", ""),
+        ("1", ""),
+    ],
+)
+def test_mask_account_card_error(value, expected):
+    with pytest.raises(AssertionError) as exc_info:
+        assert mask_account_card(value) == ('Не достаточно данных')
+
+
+@pytest.mark.parametrize(
+    "value, expected", [("2024-03-11T02:26:18.671407", "11.03.2024"), ("2024-08-06", "06.08.2024")]
+)
+def test_get_date(value, expected):
+    assert get_date(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("20/12/2023", "некорректный формат даты"),
+        ("06.05.2021", "некорректный формат даты"),
+        ("двадцать второе июня сорок первого года", "некорректный формат даты"),
+        ("aa-bb-cc", "некорректный формат даты"),
+        ("", "некорректный формат даты"),
+    ],
+)
+def test_get_date_error(value, expected):
+    with pytest.raises(AssertionError) as exc_info:
+        assert get_date(value) == expected
