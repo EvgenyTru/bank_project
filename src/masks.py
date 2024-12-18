7090967ebcc66cdf7ae3798939884c8a090d510f
Evgeny <evgeny.trufanov1984@gmail.com>
2024-12-18 19:09:50 +0400

    update func in masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,16 @@
 def get_mask_card_number(card_number: str) -> str:
     """Заменяет звездочками часть номера карты"""
-    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    if len(card_number) == 16:
+        for i in card_number:
+            if i.isdigit():
+                return f'{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}'
+    return 'Не корректный номер'
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает номер счета и возвращает маску счета"""
-
-    return f"**{account_number[-4:]}"
+    if len(account_number) == 20:
+        for i in account_number:
+            if i.isdigit():
+                return f"**{account_number[-4:]}"
+    return 'Не корректный номер'
