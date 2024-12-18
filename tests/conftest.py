eaa3fd446c295fa01bdc61048743307ebb9aa7e1
Evgeny <evgeny.trufanov1984@gmail.com>
2024-12-18 20:44:55 +0400

    addition fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,3 +29,22 @@ def count_numbers_20_spaсes():
 def count_numbers_alpha():
     return "sdfghtjranmdodfkgkgk"
 
+
+@pytest.fixture
+def list_of_dicts_base():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+@pytest.fixture
+def list_of_dicts_bad_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03"},
+        {"id": 939719570, "state": "EXECUTED", "date": "30.06.2018"},
+        {"id": 594226727, "state": "CANCELED", "date": "дата"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018"},
+    ]
+
