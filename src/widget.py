8bb2b89928f919a8919c1e2c50138266c9d6b4c9
Evgeny <evgeny.trufanov1984@gmail.com>
2024-12-18 16:09:56 +0400

    debug func mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(bank_info: str) -> str:
@@ -6,9 +6,10 @@ def mask_account_card(bank_info: str) -> str:
     if "Счет" in bank_info:
         return "Счет" + " " + get_mask_account(bank_info[5:])
     else:
-        return str(f'{bank_info[:-16]} {get_mask_card_number(bank_info[-16:])}')
+        return str(f'{bank_info[:-16]}{get_mask_card_number(bank_info[-16:])}')
 
 
-def get_date(date: str) -> str | None:
-    """Форматирование даты в нужный вид"""
-    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+print(mask_account_card('Maestro 1596837868705199'))
+# def get_date(date: str) -> str | None:
+#     """Форматирование даты в нужный вид"""
+#     return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
