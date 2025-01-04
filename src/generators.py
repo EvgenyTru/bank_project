44f3cc787d062776a6c66aa2fc77244cff8991e3
Evgeny <evgeny.trufanov1984@gmail.com>
2025-01-04 12:16:56 +0400

    fixed func and tests, update conftest.py

--- src/generators.py
+++ src/generators.py
@@ -17,14 +17,9 @@ def transaction_descriptions(transaction_list: list[dict]) -> Generator:
 
 
 def card_number_generator(start: int, finish: int) -> Generator:
-    """Генерирует 16-значные номера карт в рамках заданнного диапазона. При вызове генератора первым аргументом
+    """Генерирует 16-значные номера карт в рамках заданного диапазона. При вызове генератора первым аргументом
     передаем начальное значение генерации диапазона, вторым аргументом передаем конечное значение диапазона
     (включительно)"""
-    blank_number = "0000000000000000"
     for card_number in range(start, finish + 1):
-        card_number = int(card_number)
-        return_number = blank_number[: -len(card_number)] + card_number
-        return_number = (
-            return_number[:4] + " " + return_number[4:8] + " " + return_number[8:12] + " " + return_number[12:]
-        )
-        yield return_number
+        card_number = str(card_number).zfill(16)
+        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
